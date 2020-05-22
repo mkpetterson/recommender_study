00ad357fdd391c2b7829d9a2e456e519367c13e8
Cindy Wong <cwong690@gmail.com>
2020-05-22 11:22:05 -0700

    continue similarity function and new updated users.csv

--- src/cindy.py
+++ src/cindy.py
@@ -1,3 +1,24 @@
+class MovieRecommender():
+    """Template class for a Movie Recommender system."""
+
+    def __init__(self):
+        """Constructs a MovieRecommender"""
+        self.logger = logging.getLogger('reco-cs')
+        self.model = ALS(userCol='user',
+                itemCol='movie',
+                ratingCol='rating',
+                nonnegative=True,
+                regParam=0.1,
+                rank=10)
+        
+        self.user_factors_df = pd.read_csv('data/user_factors.csv', index_col='id')
+        self.movie_factors_df = pd.read_csv('data/movie_factors.csv', index_col='id')
+        
+        
+        # ADDITIONAL CODE FOR CSV TO ADD TO RECOMMENDER.PY
+        self.movies_sim_mat = pd.read_csv('data/movies_sim_mat.csv', index_col='movie_id')
+        self.users_sim_mat = pd.read_csv('data/users_sim_mat.csv', index_col='user_id')
+
 def predicted_rating(user_id, movie_id):
     """
     Gets the user and movie features from the save csv files. 
@@ -27,11 +48,13 @@ def predicted_rating(user_id, movie_id):
         return np.dot(np.array(user), np.array(item))
     else:
         return
+
                                   
+# CODE TO RECREATE MOVIES/USERS SIMILARITY MATRIX, NOT NECESSARY ONCE DOWNLOADED CSV
 def movies_sim_matrix(movies_df):
     """
     Create cosine similarity matrix for movies using a DataFrame containing movies information.
-    Item-item similarity.
+    Item-item similarity. Read in movies.csv file.
     
     Inputs:
     movies_df: pandas DataFrame
@@ -39,14 +62,20 @@ def movies_sim_matrix(movies_df):
     Outputs:
     movies_sim_mat: pandas DataFrame
     """
+    movies_df.set_index('movie_id', inplace=True)
+                                  
     tf = TfidfVectorizer()
     movies_tfidf = tf.fit_transform(movies_df['genre'])
     movies_indices = pd.Series(movies_df.index)
+    movies_sim_mat = pd.DataFrame(cosine_similarity(movies_tfidf), index=movies_indices,
+                                  columns=movies_indices)
+    movies_sim_mat.to_csv('data/movies_sim_mat.csv')
+    
 
 def users_sim_matrix(users_demo_df):
     """
     Create cosine similarity matrix for users using a DataFrame containing user demographic information.
-    User-user similarity.
+    User-user similarity. Read in users.csv file.
     
     Inputs:
     users_demo_df: pandas DataFrame
@@ -54,7 +83,10 @@ def users_sim_matrix(users_demo_df):
     Outputs:
     users_sim_mat: pandas DataFrame
     """
+
                                   
+                                  
+# FINDING SIMILIARTY CODE TO ADD TO RECOMMENDER.PY
 def find_similar_users(user_id):
     """
     Finds similar users and returns best guess for cosine similarity matrix
